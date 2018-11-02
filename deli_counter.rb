ae6494f8138ed393ce94c200ba51b29c73e95182
kcluniesa <kcluniesa@yahoo.com>
2018-11-02 00:09:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+   else
+     newarray = []
+     array.each.with_index do |name, index|
+       newarray << "#{index + 1}. #{name}"
+  end
+   puts "The line is currently: #{newarray.join(" ")}"
+  end
+end
